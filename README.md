a386b653a8f4df8109d88deec0f5f08300249f34
Daniela Garcia <danielagarcia9@gmail.com>
2023-06-23 22:55:14 -0400

    Projects added

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# danielagarcia9.github.io
-
+# www.dgarciaromani.com
 Welcome to my personal website, a project born out of my interest in web development. From the very beginning, I was driven by the desire to create a platform that authentically represents who I am and demonstrates my proficiency in UX/UI design and web development, all while avoiding the use of pre-made templates. I took it upon myself to meticulously design and program this website from scratch, harnessing the power of React. Every aspect of this website has been carefully crafted to reflect my personal touch and unwavering commitment to detail.
 
 ## Resources used:
--- src/data/ProjectsInfo.js
+++ src/data/ProjectsInfo.js
@@ -36,8 +36,7 @@ export const PROJECTS = [
     {
       id: 1,
       title: "My personal website",
-      tags: [
-        "HTML/CSS", "JavaScript", "Programming", "React"],
+      tags: ["Git/GitHub", "HTML/CSS", "JavaScript", "Programming", "React"],
       preview: "I have personally designed and programmed this website from scratch, utilizing React, as a platform to demonstrate my proficiency in UX/UI design and web development.",
       description: [
         "Welcome to my personal website, a project born out of my interest in web development. From the very beginning, I was driven by the desire to create a platform that authentically represents who I am and demonstrates my proficiency in UX/UI design and web development, all while avoiding the use of pre-made templates. I took it upon myself to meticulously design and program this website from scratch, harnessing the power of React. Every aspect of this website has been carefully crafted to reflect my personal touch and unwavering commitment to detail. ",
@@ -58,7 +57,7 @@ export const PROJECTS = [
     {
       id: 2,
       title: "Social Health Insights",
-      tags: ["HTML/CSS", "JavaScript", "MySQL", "Nodejs", "Programming", "React"],
+      tags: ["Git/GitHub", "HTML/CSS", "JavaScript", "MySQL", "Nodejs", "Programming", "React"],
       preview: "This collaborative project focused on synthesize Social Determinants of Health data and health outcomes into an easily accessible website, showing disparities in opportunities across different areas of the United States.",
       description: [
         "Social Determinants of Health encompass the conditions under which individuals are born, grow, live, work, and age, and the impact of these conditions on their overall health and well-being. These determinants include various factors such as income, education, employment, housing, and social support. Extensive research has consistently shown that social determinants of health significantly influence individuals' health outcomes, with those experiencing disadvantage in these areas being at a higher risk of poor health and chronic diseases. Understanding and addressing social determinants of health is crucial for improving population health overall and achieving health equity.",
@@ -80,9 +79,12 @@ export const PROJECTS = [
       id: 3,
       title: "Data Analyzer Program",
       tags: ["Programming", "Java"],
-      preview: "I'm currently working on this and making updates. Please check back at a later time to see the latest changes and updates.",
+      preview: "As part of my coursework, I designed and developed a Java application that could identify flu-related tweets, determine their origin, and analyze the data using specific criteria. This project provided me with valuable experience in software design, utilizing a modular N-tier architecture and design patterns, data analysis, and working with real-world datasets.",
       description: [
-        "I'm currently working on this and making updates. Please check back at a later time to see the latest changes and updates.",
+        "As part of my Data Structures and Software Design course, I had the opportunity to work on a project that involved designing and developing a Java application for analyzing Twitter data related to the spread of infectious diseases, specifically the flu.",
+        "The objective was to create an application that could analyze a collection of tweets, identify flu-related tweets based on specific criteria, and determine the States in which these tweets originated. The input data consisted of two formats: tab-separated text files (CSV) and JSON files. The program needed to automatically select the appropriate parser based on the file type. Additionally, a file containing the latitude and longitude of each State was provided to help determine the State of origin for each tweet.",
+        "I designed a modular program by using a 3-tier architecture, splitting up the program into three subsystems: the data tier, which handles functionality related to storing and retrieving data; the logic tier, where the necessary calculations are performed; and the user interface tier, which interacts with the user. Additionally, I created different utility classes to bundle information to be passed between different tiers and a main class to create objects in the other tiers and establish their relationships, such as passing one object to the constructor of another and initiallize te application through the user interface.",
+        "Overall, this project provided me valuable experience in software design, data analysis, and working with real-world data sets.",
       ],
       link: "",
       image: "https://fakeimg.pl/200x100?text=Image+not+available",
@@ -137,11 +139,14 @@ export const PROJECTS = [
     },
     {
       id: 5,
-      title: "Naval Clash Game",
+      title: "Battleship Game",
       tags: ["Programming", "Java"],
-      preview: "I'm currently working on this and making updates. Please check back at a later time to see the latest changes and updates.",
+      preview: "As part of my master's coursework, I created a Java version of Battleship. Players face off against the computer, aiming to sink ten randomly placed ships on a 10x10 ocean. This project helped me enhance my skills in object-oriented programming.",
       description: [
-        "I'm currently working on this and making updates. Please check back at a later time to see the latest changes and updates.",
+        'As part of my Introduction to Software Development course, I had to create a Java version of the classic game Battleship using Eclipse. In this version, the player is up against the computer, which randomly places ten ships on a 10x10 "ocean." The goal is to sink all the ships using as few shots as possible. The game follows familiar rules, starting with a grid of dots representing the ocean and providing feedback on each shot, whether the player hits or misses. Different types of ships are included, such as battleships, cruisers, destroyers, and submarines, each with its own length and characteristics.',
+        "The program utilizes the console to display instructions, the game board, and prompts the user for input (row and column to shoot). Once submitted, the computer provides feedback (hit or miss). When a ship is hit and sunk, the program provides information about the type of ship it was. After each shot, the computer redisplays the ocean with updated information.",
+        "To build this project, I divided it into several classes, including a main class for controlling the gameplay (creating instances), a class called Ocean, and an abstract class called Ship, with subclasses representing specific ship types. Additionally, I implemented methods for ship placement, shooting, sinking, and displaying the ocean. I also created JUnit test classes to ensure that the Ship and Ocean classes function correctly.",
+        "Through this project, I gained valuable experience in object-oriented programming, including inheritance and overriding, access modifiers, abstract classes, 2-dimensional array manipulation, and game logic implementation.",
       ],
       link: "",
       image: "https://fakeimg.pl/200x100?text=Image+not+available",
@@ -149,6 +154,22 @@ export const PROJECTS = [
     },
     {
       id: 6,
+      title: "Tower Blaster Game",
+      tags: ["Programming", "Python"],
+      preview: "As part of my master's coursework, I implemented a Python version of Tower Blaster,  a game where bricks are rearranged to form an increasing sequence. This project enhanced my Python skills and game development expertise.",
+      description: [
+        "As part of my Introduction to Software Development course, I implemented a Python version of the game Tower Blaster using PyCharm. Tower Blaster is a game where the player rearranges a group of bricks to create an increasing sequence. In this project, my task was to develop the game for a user versus computer scenario.",
+        "The game starts with a main pile of 60 bricks, numbered from 1 to 60, represented as a list. The objective is to arrange 10 bricks in ascending order in your own tower. At the beginning of the game, both the user and the computer are dealt 10 bricks from the main pile, following the convention of placing the bricks on top of each other. During the game, players take turns choosing a brick either from the discard pile (which is face up) or from the main pile (which is face down). The chosen brick is then placed in the player's tower, replacing an existing brick. If a brick is taken from the main pile and rejected, it is placed in the discard pile without affecting the player's tower.",
+        "Another part of the project was to design the computer's strategy. In my case, I programmed the computer to take the top brick from the discard pile and place it in a specific section of the tower. The goal was to develop a strategy that ensures the computer doesn't always lose to a human player.",
+        "To implement this project, I utilized different functions to create the main and discard piles, shuffle the bricks at the beginning of the game, check the availability of bricks in the main pile, and handle shuffling if necessary. I also implemented functions to determine if stability has been achieved in a tower, remove and return the top brick from a given pile, deal initial bricks to the user and the computer, add a brick to the discard pile, find and replace a brick in a tower, and implement the computer's decision-making process.",
+        "Completing this project provided me with a deeper understanding of working with lists and tuples in Python, as well as enhanced my problem-solving skills and logical thinking in game development scenarios.",
+      ],
+      link: "",
+      image: "https://fakeimg.pl/200x100?text=Image+not+available",
+      photos: [],
+    },
+    {
+      id: 7,
       title: "My UC",
       tags: ["Figma", "UX/UI Design"],
       preview: "This project showcases my UX/UI skills applied to a tangible problem space. The app I designed is specifically tailored to address the everyday questions and concerns of university students by providing them with comprehensive information.",
